86180c8cb5aff422ea4493ce38ea024258ab1f53
ilikecubesnstuff <25328250+ilikecubesnstuff@users.noreply.github.com>
2024-01-22 01:45:30 +0000

    :bug: Move galpy import to an import extension.

--- src/pidgey/galpy_backend.py
+++ src/pidgey/galpy_backend.py
@@ -1,7 +1,6 @@
 import astropy.coordinates as coord
 import astropy.units as u
 import numpy as np
-from galpy.util.conversion import get_physical
 
 from .base import Backend
 
@@ -9,6 +8,7 @@ from .base import Backend
 class GalpyBackend(Backend):
     def __imports__():
         from galpy import orbit
+        from galpy.util.conversion import get_physical
 
     def ORBIT_TYPE(self):
         return self.orbit.Orbit
@@ -17,7 +17,7 @@ class GalpyBackend(Backend):
         self, skycoord, pot, dt, steps, pattern_speed=0 * u.km / u.s / u.kpc
     ):
         # unit consistency issues... look into this later
-        pot_units = get_physical(pot)
+        pot_units = self.get_physical(pot)
         orbit = self.orbit.Orbit(skycoord, **pot_units)
 
         t = np.arange(steps) * dt
