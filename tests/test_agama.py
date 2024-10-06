49e90fd9e0e1ce5a3c0c5e7fc208b29a21f72195
ilikecubesnstuff <25328250+ilikecubesnstuff@users.noreply.github.com>
2024-10-07 01:37:27 +0200

    :white_check_mark: Fix Agama testing to a particular commit. :white_check_mark: Comment out test for specifying the integrator, since that is only supported in later Agama versions.

--- tests/test_agama.py
+++ tests/test_agama.py
@@ -47,27 +47,28 @@ def test_single_orbit(potential, backend):
     assert len(orbit) == STEPS
 
 
-def test_single_orbit_with_alternate_integrator(potential, backend):
-    STEPS = 50
-    c = coord.SkyCoord(
-        ra=20.0 * u.deg,
-        dec=30.0 * u.deg,
-        distance=2.0 * u.kpc,
-        pm_ra_cosdec=-10.0 * u.mas / u.yr,
-        pm_dec=20.0 * u.mas / u.yr,
-        radial_velocity=50.0 * u.km / u.s,
-        galcen_distance=8.0 * u.kpc,
-        z_sun=15.0 * u.pc,
-        galcen_v_sun=CartesianDifferential([10.0, 235.0, 7.0] * u.km / u.s),
-    )
-    orbit = backend.compute_orbit(
-        c, potential, dt=0.1 * u.Gyr, steps=STEPS, method="hermite"
-    )
-    assert isinstance(orbit, coord.representation.CartesianRepresentation)
-    assert len(orbit) == STEPS
-    orbit = backend.get_points()
-    assert isinstance(orbit, coord.representation.CartesianRepresentation)
-    assert len(orbit) == STEPS
+# for newer versions of agama
+# def test_single_orbit_with_alternate_integrator(potential, backend):
+#     STEPS = 50
+#     c = coord.SkyCoord(
+#         ra=20.0 * u.deg,
+#         dec=30.0 * u.deg,
+#         distance=2.0 * u.kpc,
+#         pm_ra_cosdec=-10.0 * u.mas / u.yr,
+#         pm_dec=20.0 * u.mas / u.yr,
+#         radial_velocity=50.0 * u.km / u.s,
+#         galcen_distance=8.0 * u.kpc,
+#         z_sun=15.0 * u.pc,
+#         galcen_v_sun=CartesianDifferential([10.0, 235.0, 7.0] * u.km / u.s),
+#     )
+#     orbit = backend.compute_orbit(
+#         c, potential, dt=0.1 * u.Gyr, steps=STEPS, method="hermite"
+#     )
+#     assert isinstance(orbit, coord.representation.CartesianRepresentation)
+#     assert len(orbit) == STEPS
+#     orbit = backend.get_points()
+#     assert isinstance(orbit, coord.representation.CartesianRepresentation)
+#     assert len(orbit) == STEPS
 
 
 def test_single_orbit_with_pattern_speed(potential, backend):
